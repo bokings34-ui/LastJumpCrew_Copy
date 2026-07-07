4a71e854d4bc463e867716b25aa9cc87c4c9e317
Hellex <bokings34@gmail.com>
2026-07-07 15:45:09 +0900

    중간저장

--- Assets/03. SeoBoGyeong_Game Economy/02. Script/SO/ZoneData.cs
+++ Assets/03. SeoBoGyeong_Game Economy/02. Script/SO/ZoneData.cs
@@ -4,11 +4,13 @@ using UnityEngine;
     fileName = "Zone_New",
     menuName = "Game Data/Zone")]
 
-public class ZoneData:ScriptableObject
+public class ZoneData:ScriptableObject,IGameData
 {
     [Header("Zone Info")]
     public int id;
     public string zoneName;
+
+    int IGameData.Id => id;
 
     // 추가되야할 정보 : 이벤트 확률, 보상, 난이도 등
 }
