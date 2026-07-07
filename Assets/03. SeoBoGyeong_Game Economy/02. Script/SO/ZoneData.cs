3bd3609ab01af9b59f31310e9317f0e3752faa67
Hellex <bokings34@gmail.com>
2026-07-07 14:15:42 +0900

    Update ZoneData.cs
    2차

--- Assets/03. SeoBoGyeong_Game Economy/02. Script/SO/ZoneData.cs
+++ Assets/03. SeoBoGyeong_Game Economy/02. Script/SO/ZoneData.cs
@@ -4,8 +4,11 @@ using UnityEngine;
     fileName = "Zone_New",
     menuName = "Game Data/Zone")]
 
-public class ZoneData
+public class ZoneData:ScriptableObject
 {
+    [Header("Zone Info")]
     public int id;
-    public string name;
+    public string zoneName;
+
+    // 추가되야할 정보 : 이벤트 확률, 보상, 난이도 등
 }
