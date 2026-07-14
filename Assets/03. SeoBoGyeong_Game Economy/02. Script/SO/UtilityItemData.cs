e16c70cc4a2a7e55edd2eed3ebcf30ecdbfe9c46
Hellex <bokings34@gmail.com>
2026-07-14 11:45:52 +0900

    Update UtilityItemData.cs

--- Assets/03. SeoBoGyeong_Game Economy/02. Script/SO/UtilityItemData.cs
+++ Assets/03. SeoBoGyeong_Game Economy/02. Script/SO/UtilityItemData.cs
@@ -8,8 +8,13 @@ namespace LastJumpCrew.SeoBoGyeong.Data
     public class UtilityItemData : ItemData
     {
         [Header("아이템 타입")]
-        [SerializeField] private ItemType type;
-        // 조한용님(06)의 전역 ItemType enum 사용
+        [SerializeField] private ItemType nomarlType;
+        [SerializeField] private ItemType actType;
+        // 조한용님(06)의  ItemType enum 사용
+
+        [Header("수리량")]
+        [Tooltip("수리에 적용되는 값")]
+        [SerializeField] private float value;
 
         [Header("내구도")]
         [Tooltip("내구도 사용 여부")]
@@ -19,5 +24,7 @@ namespace LastJumpCrew.SeoBoGyeong.Data
 
         public bool HasDurability => hasDurability;
         public int MaxDurability => maxDurability;
+
+        public float Value => value;
     }
 }
