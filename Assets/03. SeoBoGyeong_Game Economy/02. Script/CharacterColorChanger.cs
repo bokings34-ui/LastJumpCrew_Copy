5dd0d0f6be4b4e28ad607c59734494d8a87b3b2d
Hellex <bokings34@gmail.com>
2026-07-29 10:08:48 +0900

    Add 캐릭터 fbx

---
+++ Assets/03. SeoBoGyeong_Game Economy/02. Script/CharacterColorChanger.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+public class CharacterColorChanger : MonoBehaviour
+{
+    [SerializeField] private SkinnedMeshRenderer target;
+    [SerializeField] private Color baseColor = Color.white;
+
+    private Material[] mats;
+
+    private void Start()
+    {
+        mats = target.materials;
+
+    }
+
+    [ContextMenu("Change Color")]
+    private void ChangeColor()
+    {
+        for (int i = 0; i < mats.Length; i++)
+        {
+            mats[i].color = baseColor;
+        }
+    }
+
+    public void SetColor(Color color)
+    {
+        foreach (Material mat in mats)
+        {
+            mat.color = color;
+        }
+    }
+}
+
