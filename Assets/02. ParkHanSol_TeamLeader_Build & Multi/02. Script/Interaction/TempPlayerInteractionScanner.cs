cf47add26090d55c51b3c6e938e30f85e4c08294
JoHanyong <dlqrk21@gmail.com>
2026-08-06 10:43:39 +0900

    우클릭 좌클릭 분리 + 넉백 최대값 8->15
    우클릭 감전 투척하고 투척하고 있던 걸 우클릭 좌클릭으로 원상복구하고 플레이어가 받을 수 있는 최대 넉백값을 8에서 15로 수정했습니다

--- Assets/02. ParkHanSol_TeamLeader_Build & Multi/02. Script/Interaction/TempPlayerInteractionScanner.cs
+++ Assets/02. ParkHanSol_TeamLeader_Build & Multi/02. Script/Interaction/TempPlayerInteractionScanner.cs
@@ -535,13 +535,6 @@ namespace LastJumpCrew.ParkHanSol.Interaction
                 return;
             }
 
-            var heldItemData = GetComponent<TempPlayerItemHolder>()?.CurrentItemPrefabData;
-            if (heldItemData != null && heldItemData.ItemId == "battery_pack")
-            {
-                combatController.RequestBatteryThrow();
-                return;
-            }
-
             combatController.RequestThrowHeldItem(heldDuration);
         }
         
